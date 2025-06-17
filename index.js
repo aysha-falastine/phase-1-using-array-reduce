bd1bc4759876dac2dd279a9afbdb7c449ace8ff7
asha mohamed <falastine924@gmail.com>
2025-06-17 10:22:41 +0300

    index.html

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const batteries = [
+  { type: "AA", assembled: 5 },
+  { type: "AAA", assembled: 12 },
+  { type: "AA", assembled: 4 },
+  { type: "AAA", assembled: 10 }
+];
+
+const totalBatteries = batteries.reduce((total, battery) => {
+  return total + battery.assembled;
+}, 0);
